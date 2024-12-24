cf508987dd7194f714017884c0778e705a80e24a
ɐllɐ <molchanova.a.a@gmail.com>
2024-12-24 12:08:27 +0300

    task 18.1.1

--- SkillFactoryCSharp18ExtraPatterns/Program.cs
+++ SkillFactoryCSharp18ExtraPatterns/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BuilderTask;
+using System.Text;
+namespace BuilderTask
+{
+    class Program
+    {
+        /// <summary>
+        /// Клиентский код
+        /// </summary>
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
 
+            // Объект завода, который будет заниматься производством
+            var carPlant = new CarPlant();
+
+            // производство скутеров
+            Conveyor builder = new ScooterConveyor();
+            carPlant.Construct(builder);
+            builder.Product.Show();
+
+            // производство автомобилей
+            builder = new CarConveyor();
+            carPlant.Construct(builder);
+            builder.Product.Show();
+
+            // производство мотоциклов
+            builder = new MotoConveyor();
+            carPlant.Construct(builder);
+            builder.Product.Show();
+        }
+    }
+}
